1da7cd1725932b39d446c9e50dd0992971cd3f2b
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-15 17:24:07 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,5 +1,11 @@
-ratelimit:
-  limite-documental: 300
-  n: 2
-  margem: 0.9
-  reserva: 20
+# opção A - lista YAML
+hazelcast:
+  port: 5701
+  members:
+    - servidor-a.qab.interno
+    - servidor-b.qab.interno
+
+# opção B - string separada por vírgula
+hazelcast:
+  port: 5701
+  members: "servidor-a.qab.interno,servidor-b.qab.interno"
