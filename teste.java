b5ab380f9b80b789ca99c70d4a914e1f6fd0ec55
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 08:06:11 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,1 +1,2 @@
-ls code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/ 2>/dev/null && echo "TEM TESTE ZUMBI" || echo "OK sem teste zumbi"
+git add -A code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/
+git add -A code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/
