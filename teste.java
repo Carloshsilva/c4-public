5e278c8b7f70c57e360930b36b5cce224efc196b
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 08:18:43 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,1 +1,2 @@
-grep -rln "outbound.ratelimit" code/src/
+git checkout cefef03 -- code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
+git checkout cefef03 -- code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ValidationServiceTestConfiguration.java
