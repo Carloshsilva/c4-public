69fd7ad7d014f76b3d6b732284eb7ef0836a81cd
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 08:17:29 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,2 +1,1 @@
-git add -A code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/
-git add -A code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/
+grep -rln "outbound.ratelimit" code/src/
