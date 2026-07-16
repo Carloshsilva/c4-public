d1863c60350d8250d2ec0f253832cad8aa10cf17
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 07:07:15 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,2 +1,14 @@
-ls code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/
-  git ls-tree --name-only HEAD code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/
+git checkout --theirs code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/configs/HazelcastConfig.java
+git checkout --theirs code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiter.java
+git checkout --theirs code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/JsonRestClient.java
+git checkout --theirs code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ProcessingService.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/full/FullTest.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiterTest.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/services/ProcessingServiceTest.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ConcurrencyLimiterTestConfiguration.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomControllersConfiguration.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/FullTestConfiguration.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/OutboundControllerV1TestConfiguration.java
+git checkout --theirs code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ProcessingServiceTestConfiguration.java
+
+  git add code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/configs/HazelcastConfig.java code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiter.java code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/JsonRestClient.java code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ProcessingService.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/full/FullTest.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiterTest.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/services/ProcessingServiceTest.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ConcurrencyLimiterTestConfiguration.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomControllersConfiguration.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/FullTestConfiguration.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/OutboundControllerV1TestConfiguration.java code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ProcessingServiceTestConfiguration.java
