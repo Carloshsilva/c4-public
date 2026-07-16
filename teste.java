0b1beff227090d05f67a7803c7da5d6c53d4eeac
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 08:31:03 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,2 +1,1 @@
-grep -n "import.*ratelimit" code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
-grep -n "Relogio\|RateLimiter" code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
+grep -n "Relogio\|RateLimiter\|ratelimit" code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ValidationServiceTestConfiguration.java
