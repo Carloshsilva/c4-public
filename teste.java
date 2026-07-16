eb97741c496e18bf4dbffc91c8b36d0e54f97991
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-15 23:46:12 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,20 +1,2 @@
-git checkout --ours code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/configs/HazelcastConfig.java
-git checkout --ours code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiter.java
-git checkout --ours code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/JsonRestClient.java
-git checkout --ours code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ProcessingService.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/full/FullTest.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiterTest.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/services/ProcessingServiceTest.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ConcurrencyLimiterTestConfiguration.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomControllersConfiguration.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/FullTestConfiguration.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/OutboundControllerV1TestConfiguration.java
-git checkout --ours code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ProcessingServiceTestConfiguration.java
-
-git add code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/configs/HazelcastConfig.java
-git add code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/services/ConcurrencyLimiter.java
-# ... (todos os que você acabou de fazer checkout --ours)
-
-git add code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/
-git add code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/
-  
+ls code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/ratelimit/
+  git ls-tree --name-only HEAD code/src/main/java/br/com/b3/middlewares/selicconecta/outbound/
