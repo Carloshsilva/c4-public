14fb8d3823f2bc1acfb133c551a88ccb85489157
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 08:21:38 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,2 +1,2 @@
-git checkout cefef03 -- code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
-git checkout cefef03 -- code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/ValidationServiceTestConfiguration.java
+git log --oneline origin/develop -- code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
+  
