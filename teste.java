61fa11baf568ad421cad92deb3e509d5114dcf1f
Carlos Henrique Alves Silva <email@carloshsilva.com>
2026-07-16 08:29:17 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,1 +1,2 @@
-git diff cefef03 HEAD -- code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
+grep -n "import.*ratelimit" code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
+grep -n "Relogio\|RateLimiter" code/src/test/java/br/com/b3/middlewares/selicconecta/outbound/tests/config/CustomServicesConfiguration.java
